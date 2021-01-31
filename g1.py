85705899f7460822eae7d309e1d738a9a0cd91b8
Daniel Kelling <D-to-the-K@users.noreply.github.com>
2021-01-31 20:52:32 -0300

    Updated run command to obey robots.txt; removed unnecessary check

--- g1.py
+++ g1.py
@@ -1,5 +1,5 @@
 # running:
-# scrapy runspider g1.py -O output.json
+# scrapy runspider g1.py -s ROBOTSTXT_OBEY=True -o output.json
 
 import scrapy
 import re
@@ -13,18 +13,17 @@ class Spider(scrapy.Spider):
     link_extractor = LinkExtractor(allow=['\/noticia\/', '\.ghtml$'])
 
     def parse(self, response):
-        # If this is an article, get data from it
-        if response.xpath('//article') is not None:
-            yield {
-                'bot_name': self.name,
-                'url': response.url,
-                'title': response.xpath('//meta[@name="title"]/@content').get(),
-                'description': response.xpath('//meta[@name="description"]/@content').get(),
-                'content': ''.join(response.xpath('//article').xpath('.//*/text()').getall()),
-                'date': response.xpath('//time[@itemprop="datePublished"]/@datetime').get(),
-                'author': response.xpath('//p[@class="content-publication-data__from"]/@title').get(),
-            }
+        # Get data from this article
+        yield {
+            'bot_name': self.name,
+            'url': response.url,
+            'title': response.xpath('//meta[@name="title"]/@content').get(),
+            'description': response.xpath('//meta[@name="description"]/@content').get(),
+            'content': ''.join(response.xpath('//article').xpath('.//*/text()').getall()),
+            'date': response.xpath('//time[@itemprop="datePublished"]/@datetime').get(),
+            'author': response.xpath('//p[@class="content-publication-data__from"]/@title').get(),
+        }
 
-        # Follow more news links
+        # Follow other article links
         for link in self.link_extractor.extract_links(response):
             yield scrapy.Request(link.url, callback=self.parse)
