4d4b1973bb0982d78c79bf8db18cc26a0f0fd657
Matheus Westhelle <matheus.westhelle@gmail.com>
2021-03-07 23:02:37 -0300

    Add ElasticSearch ingest pipeline

--- witms/pipelines.py
+++ witms/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 # from itemadapter import ItemAdapter
+from elasticsearch import Elasticsearch, helpers
+
+BUFFER_SIZE = 1000
 
 
-class WitmsPipeline:
+class ElasticSearchPipeline:
+    def __init__(self):
+        self.items_buffer = []
+
+    def open_spider(self, spider):
+        self.client = Elasticsearch(hosts=["localhost"])
+
     def process_item(self, item, spider):
+        action = {"_index": "news-index", "_source": dict(item)}
+        self.items_buffer.append(action)
+        if len(self.items_buffer) >= BUFFER_SIZE:
+            helpers.bulk(self.client, self.items_buffer)
+            self.items_buffer = []
         return item
